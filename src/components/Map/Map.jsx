20c63ef2f7eabbae6a252eadbe2665b634043997
sowabdulai <abdulai-as@hotmail.com>
2022-02-05 08:44:34 -0500

    map css & import

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typograghy, useMediaQuery } from '@meterial-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/locationOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
   return <h1>Map</h1>;
