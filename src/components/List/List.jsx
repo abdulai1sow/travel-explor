260768e4864fe6bba8cfac813128a9bafea87156
sowabdulai <abdulai-as@hotmail.com>
2022-02-06 21:29:22 -0500

    working on list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import { CirclarProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import useStyles from './styles';
 
 const List = () => {
-  return <h1>List</h1>;
+  const classes = useStyles();
+  const [type, setType] = useState();
+
+  return
+  <div className={classes.container}>
+    <Typography variant="h4">
+      Resturants, Hotels & Attractions arount you
+    </Typography>
+    <FormControl className={classes.formControl}>
+      <InputLabel>Type</InputLabel>
+      <Select value={""} onChange={}>
+        <ManuItem value="resturants"> Resturants</ManuItem>
+        <ManuItem value="hotels"> Hotels</ManuItem>
+        <ManuItem value="attractions"> Attractions</ManuItem>
+      </Select>
+    </FormControl>
+  </div>;
 };
 
 export default List;
